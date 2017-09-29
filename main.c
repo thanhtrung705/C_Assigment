0ff44dbfad314ba6aec2bf82c635a0e7e11808b1
trung dinh <trungdtd00548@fpt.edu.vn>
2017-09-29 21:35:23 +0700

    Quan ly sinh vien

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+/*
+ *Created by TRUNGDT
+ * 29/09/2017.
+ * Version x.x.x
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -8,30 +13,39 @@ struct GoStudents {      //Khai báo cấu trúc sinh viên
     char PhoneNumber[100];
     char ID[100];
 };
-typedef struct GoStudents Students;// Gán cấu trức sinhvien thành kiểu dữ liệu SinhVien
+typedef struct GoStudents Students;// Gán cấu trức GoStudents thành kiểu dữ liệu SinhVien
 Students sv[100];//Khai báo mảng sv ghi thông tin số lương sinh viên
-int n;//số lượng sinh viên
+int n; //số lượng sinh viên
 unsigned int leng;
-
-bool checkExistID() {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (strcmp(sv[j].ID, sv[i].ID)==0) {//Vòng lặp so sáng giữa mảng ID lần lượt để tìm ra giá trị trùng
-                printf("ID is exist.Please enter different ID\n");
-                return true;//Nếu trùng hàm trả về false thực hiện tiếp nhập lại ID
-            }
+int counter;
+int x;
+//Kiem tra ID da ton tai hay chua.
+bool checkExistID(char a[], int k) {
+    for (int i = 0; i < k; i++) {
+        if (strcmp(sv[i].ID, a) == 0) {
+            printf("ID is existed.Please enter different ID\n");
+            return true;
         }
     }
-    return false;//Nếu ktra ok hàm trả về false, kết thúc vòng lặp, chuyển sang nhập thông tin kế tiếp
-}
-int checkIndexID(char a[]){//Check vị trí của ID vừa khai báo
-    for(int i = 0; i<n; i++) {
-        if (strcmp(sv[i].ID, a) == 0)//Thực hiện vòng lặp so sáng giá trị ID từ 0->n so với giá trị a là ID nhập vào
-            return i;                   //Nếu giá trị bằng 0 trả về i là index của ID đó
-    }
+    return false;
 }
 
-void checkName(int a){//Check độ dài tên
+//Check vị trí của ID vừa khai báo
+int checkIndexID() {
+    char buffer[100];
+    int i;
+    do {
+        printf("Please enter ID to edit/delete:\n");
+        fgets(buffer, 100, stdin);
+        for (i = 0; i < (n + counter); i++) {
+            if (strcmp(sv[i].ID, buffer) == 0)//Thực hiện vòng lặp so sáng giá trị ID từ 0->n so với giá trị a là ID nhập vào
+                return i;
+        }
+        printf("ID is not exist!!!\n");
+    } while (strcmp(sv[i].ID, buffer) != 0);
+}
+
+void checkName(int a) {//Check độ dài tên
     if (a - 1 < 3)
         printf("Name student is too short. Name student have to larger than 3 characters\n");
     else if (a - 1 > 15)
@@ -40,86 +54,109 @@ void checkName(int a){//Check độ dài tên
 
 void WriteFile() {
     FILE *pt;
-    pt = fopen("E:\\C-semester1\\INPUT.txt", "w");
-    for (int i = 0; i < n; i++) {
-        fprintf(pt, "\nStudent information number %i\n", i + 1);
-        fputs("ID: ", pt);
+    if(counter == 0)//mở file với chế độ ghi khi file đang trắng
+        pt=fopen("E:\\C-semester1\\INPUT.txt", "w");
+    else
+        pt = fopen("E:\\C-semester1\\INPUT.txt", "a");//mở file khi file có sẵn dữ liệu, áp dụng khi mở file lần chạy compile sau
+    for (int i = counter; i <  n + counter; i++) {
+        fputs("\n", pt);
         fputs(sv[i].ID, pt);
-        fputs("Student Name: ", pt);
         fputs(sv[i].Name, pt);
-        fputs("Date of Birth: ", pt);
         fputs(sv[i].DateofBirth, pt);
-        fputs("Phone number: ", pt);
         fputs(sv[i].PhoneNumber, pt);
     }
+    counter+=n;//Nhập n sau lần compile đầu tiên sẽ cộng với counter ra tổng số sinh viên đã có.
     fclose(pt);
 }
 
 void ReadFile() {
-    char str[100];
+    char buffer[100];
+    int k;
+    counter=0;//Đọc lại file từ đầu đến cuối.
     FILE *pt;
     pt = fopen("E:\\C-semester1\\INPUT.txt", "r");
-    while (fgets(str, 81, pt)) {
-        printf("%s", str);
+    for(k = 0; fgets(buffer,100, pt); k++){
+        printf("Student information number %d\n", k+1);
+        printf("Student ID: ");
+        fgets(sv[k].ID, 100, pt);
+        printf("%s", sv[k].ID);
+        printf("Name of Student: ");
+        fgets(sv[k].Name, 100, pt);
+        printf("%s", sv[k].Name);
+        printf("Date of Birth: ");
+        fgets(sv[k].DateofBirth, 100, pt);
+        printf("%s", sv[k].DateofBirth);
+        printf("Phone number: ");
+        fgets(sv[k].PhoneNumber, 100, pt);
+        printf("%s\n", sv[k].PhoneNumber);
+        counter++;//khi mở file lúc đóng compile phần mềm đếm lên để khi nhập mới sẽ điên vào nối tiếp file.
     }
     fclose(pt);
 }
 
-void AddListStudents(Students sv[]) {
+
+void AddListStudents() {
+    char buffer[100];
     printf("Please enter number of students:\n");
     scanf("%d", &n);
     getchar();
-    for (int i = 0; i < n; i++) {
+    for (int i = counter; i < (n + counter); i++) {//(n + counter): duyệt số phần từ sau lần chạy compile sau
         printf("Student information number %i\n", i + 1);
+        int k = 1;  //Kiem tra dieu kien thuc hien vong lap
         do {
             printf("Please enter student ID:(Number type)\n");
-            fgets(sv[i].ID, 100, stdin);//Gõ ID cần tạo
-            checkExistID();//gọi hàm check giá trị ID bị trùng
-        } while (checkExistID());
+            fgets(buffer, 100, stdin);
+            if (checkExistID(buffer, i) == false) {
+                strcpy(sv[i].ID, buffer);
+                k--;// k=0 trả về false, thoát vòng lặp.
+            }
+        } while (k);
         do {
             printf("Please enter student name\n");
             fgets(sv[i].Name, sizeof(sv[i].Name), stdin);
-            leng= strlen(sv[i].Name);
+            leng = strlen(sv[i].Name);
             checkName(leng);
         } while (leng - 1 > 15 || leng - 1 < 3);
         printf("Please enter date of birth:\n");
         fgets(sv[i].DateofBirth, sizeof(sv[i].DateofBirth), stdin);
         printf("Please enter phone number:\n");
         fgets(sv[i].PhoneNumber, sizeof(sv[i].PhoneNumber), stdin);
-        WriteFile();
     }
+    WriteFile();
 }
 
 void EditStudents() {
-    char buffer[100];
     printf("Edit Student Information\n");
-    printf("Please enter ID to edit:\n");
-    fgets(buffer, 100, stdin);//Gõ ID cần sửa
-    checkIndexID(buffer);//Gọi hàm để check vị trí index cần sửa
+//    printf("Please enter ID to edit:\n");
+//    fgets(buffer, 100, stdin);//Gõ ID cần sửa
+    int k = checkIndexID();//Gọi hàm để check vị trí index cần sửa
     printf("Edit Name Student:\n");
     do {
-        fgets(sv[checkIndexID(buffer)].Name, sizeof(sv[checkIndexID(buffer)].Name), stdin);//checkIndexID(buffer) là giá trị index hàm trả về
-        leng = strlen(sv[checkIndexID(buffer)].Name);
+        fgets(sv[k].Name, sizeof(sv[k].Name), stdin);//checkIndexID(buffer) là giá trị index hàm trả về
+        leng = strlen(sv[k].Name);
         checkName(leng);//Check độ dài tên
-    } while (strlen(sv[checkIndexID(buffer)].Name) - 1 > 15 || strlen(sv[checkIndexID(buffer)].Name) - 1 < 3);
+    } while (strlen(sv[k].Name) - 1 > 15 || strlen(sv[k].Name) - 1 < 3);
     printf("Edit Date of Birth:\n");
-    fgets(sv[checkIndexID(buffer)].DateofBirth, sizeof(sv[checkIndexID(buffer)].DateofBirth), stdin);
+    fgets(sv[k].DateofBirth, sizeof(sv[k].DateofBirth), stdin);
     printf("Edit Phone number:\n");
-    fgets(sv[checkIndexID(buffer)].PhoneNumber, sizeof(sv[checkIndexID(buffer)].PhoneNumber), stdin);
+    fgets(sv[k].PhoneNumber, sizeof(sv[k].PhoneNumber), stdin);
+    n = counter;//Gán giá trị đếm cho n rồi chuyển counter về 0 dể thực hiện vòng lặp ghi file từ khi counter =0.
+    counter = 0 ;
     WriteFile();
 }
 
-void DeleteStudent(char buffer[100]) {//Khai báo biến tạm để nhập ID sinh viên
+void DeleteStudent() {//Khai báo biến tạm để nhập ID sinh viên
     printf("Delete Student\n");
-    printf("Please enter Student ID to delete\n");
-    fgets(buffer, 100, stdin);//Nhập ID cần xóa
-    checkIndexID(buffer);//gọi hàm check vị trí trong mảng của ID sinh viên vừa tạo
-    for (int i = checkIndexID(buffer); i < n; i++) {//Thực hiện vòng lặp chèn vị trí sinh viên cần xóa bằng vị trí sau nó
-        sv[i] = sv[i + 1];
+    int k = checkIndexID();//gọi hàm check vị trí trong mảng của ID sinh viên vừa tạoThực hiện vòng lặp chèn vị trí sinh viên cần xóa bằng vị trí sau nó
+    n = counter;
+    counter = 0;
+    for(k; k < n; k++) {
+        sv[k] = sv[k + 1];
     }
-    n--;//Giảm vùng nhớ đi 1 giá trị sau khi xóa
+    n--;
     WriteFile();
 }
+
 
 /*void TIM_KIEM_THEO_TEN(Students sv[], char ten[100]) {
     int k = 0;
@@ -136,7 +173,6 @@ void DeleteStudent(char buffer[100]) {//Khai báo biến tạm để nhập ID s
         printf("Khong tim thay sinh vien\n");*/
 
 choice() {
-    char a[100];
     while (true) {
         char option;
         printf("\n==============MENU============\n");
@@ -152,18 +188,17 @@ choice() {
         switch (option) {
             case '1':
                 printf("Add List Students\n");
-                AddListStudents(sv);
+                AddListStudents();
                 break;
             case '2':
                 printf("Review List Students\n");
                 ReadFile();
                 break;
             case '3':
-                //printf("%d", checkIndexID(a));
                 EditStudents();
                 break;
             case '4':
-                DeleteStudent(a);
+                DeleteStudent();
                 break;
 /*            case '5':
                 printf("Tim kiem sinh vien\n");
